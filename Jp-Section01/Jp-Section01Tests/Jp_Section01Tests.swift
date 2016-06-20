e82359ec30c5ee25a4cad4791d2f56c0507c9a6d
Jp <Jp.LaFond@gmail.com>
2016-06-20 17:32:23 -0400

    Create the initial xcode project for the section.

---
+++ Jp-Section01/Jp-Section01Tests/Jp_Section01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Jp_Section01Tests.swift
+//  Jp-Section01Tests
+//
+//  Created by Jp LaFond on 6/20/16.
+//  Copyright © 2016 Jp LaFond. All rights reserved.
+//
+
+import XCTest
+@testable import Jp_Section01
+
+class Jp_Section01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
