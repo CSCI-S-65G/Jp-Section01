b9033285709d488ae78faf5e14ae8bd25f76567b
Jp <Jp.LaFond@gmail.com>
2016-06-27 16:08:24 -0400

    Save the playground, as I work on it.

---
+++ s65g-Playground/s65g-Playground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print ("\(str)")
+
+var myImplicitInt = 7
+myImplicitInt = 8
+
+var myDouble : Double = 7
+myDouble = myDouble/3
+
+let myFloat : Float = 7/3
+
+let myImplicitDouble = 2.33
+
